19c95d9766a8b3002fc362f8f0baf0be006f1c74
Eon <eon-ex.v1ad@yandex.ru>
2026-04-07 21:12:12 +0300

    исправление другой ошибки checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/MpaRatingStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/MpaRatingStorage.java
@@ -4,6 +4,8 @@ import ru.yandex.practicum.filmorate.model.MpaRating;
 import java.util.List;
 
 public interface MpaRatingStorage {
+
     List<MpaRating> getAll();
+
     MpaRating getById(Long id);
 }
